70f7554aa14c2755a18666491b7d82f541ef5414
ajeet-ahex <ajeet.ahex@gmail.com>
2024-04-29 14:28:43 +0530

    changed desc

--- ecommerce_website_minimum_order_quantity/__manifest__.py
+++ ecommerce_website_minimum_order_quantity/__manifest__.py
@@ -13,9 +13,7 @@
     'images': [
         'static/description/banner.png',
         ],
-    'Versions': [
-        '17.0',
-        ],
+        
     'data': [
         'views/products_view.xml',
         'views/website_product_view.xml',
